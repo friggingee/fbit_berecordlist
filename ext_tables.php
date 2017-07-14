0e68712796b1b74987232b67c1c01b5aeb9bbc8e
Grigori Prokhorov <hello@feinberg.it>
2017-07-14 12:25:03 +0200

    Initial commit

--- ext_tables.php
+++ ext_tables.php
@@ -1,30 +1,40 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-    function () {
+if (TYPO3_MODE === 'BE') {
+    if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['fbit_berecordlist']['modules'])) {
         if (TYPO3_MODE === 'BE') {
-            if (is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['fbit_berecordlist']['modules'])) {
-                $modulesConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXT']['fbit_berecordlist']['modules'];
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+                'fbit',
+                '',
+                'before:tools',
+                '',
+                [
+                    'access' => 'group,user',
+                    'labels' => 'LLL:EXT:fbit_berecordlist/Resources/Private/Language/locallang_mod_module.xlf',
+                    'icon' => '',
+                    'name' => null,
+                ]
+            );
+        }
 
-                foreach ($modulesConfiguration as $extKey => $moduleConfiguration) {
-                    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                        'FBIT.BeRecordList',
-                        'web',
-                        $extKey,
-                        '',
-                        [
-                            'Module' => 'list',
-                        ],
-                        [
-                            'access' => 'user,group',
-                            'icon' => $moduleConfiguration['icon'],
-                            'labels' => $moduleConfiguration['labels'],
-                        ]
-                    );
-                }
-            }
+        $modulesConfiguration = $GLOBALS['TYPO3_CONF_VARS']['EXT']['fbit_berecordlist']['modules'];
 
+        foreach ($modulesConfiguration as $extKey => $moduleConfiguration) {
+            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                'FBIT.BeRecordList',
+                'fbit',
+                $extKey,
+                '',
+                [
+                    'Module' => 'list',
+                ],
+                [
+                    'access' => 'user,group',
+                    'icon' => $moduleConfiguration['icon'],
+                    'labels' => $moduleConfiguration['labels'],
+                ]
+            );
         }
     }
-);
+}
