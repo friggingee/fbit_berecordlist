d5f2a9f013caffddc4aba6007fd9bd037ca51c90
Grigori Prokhorov <hello@feinberg.it>
2021-12-17 11:08:31 +0100

    increase version and constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.2.1',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.6.0-8.7.99',
+            'typo3' => '8.6.0-10.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
